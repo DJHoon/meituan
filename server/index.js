350b695b2a8b59715d3e12cb5a21c43dbcce80cb
DJHoon <742471208@qq.com>
2020-08-02 13:04:35 +0800

    登录注册功能

--- server/index.js
+++ server/index.js
@@ -2,8 +2,36 @@ const Koa = require('koa')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 
+const mongoose = require('mongoose')
+const bodyParser = require('koa-bodyparser')
+const session = require('koa-generic-session')
+const json = require('koa-json')
+const Redis =require('koa-redis')
+import dbConfig from './db/config'
+import passport from './interface/utils/passport'
+import users from './interface/users'
+import geo from './interface/geo'
+
 const app = new Koa()
 
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+app.keys = ['mt', 'keyskeys']
+app.proxy = true
+app.use(session({
+  key:'mt',
+  prefix: 'mt:uid',
+  store: new Redis()
+}))
+app.use(bodyParser({
+  extendTypes: ['json', 'form', 'text']
+}))
+app.use(json())
+mongoose.connect(dbConfig.db, {
+  useNewUrlParser: true
+})
+app.use(passport.initialize())
+app.use(passport.session())
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = app.env !== 'production'
@@ -23,7 +51,8 @@ async function start () {
     const builder = new Builder(nuxt)
     await builder.build()
   }
-
+  app.use(users.routes()).use(users.allowedMethods())
+  app.use(geo.routes()).use(geo.allowedMethods())
   app.use((ctx) => {
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
